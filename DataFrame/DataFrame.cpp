d985ecc345be9d5b37a387e68cb2e947b9c49684
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-12-11 21:40:00 +0530

    Dataframe and series design

---
+++ DataFrame/DataFrame.cpp
@@ -0,0 +1,13 @@
+#include"../reader/lazy_read.h"
+using namespace std;
+
+class DataFrame{
+    public:
+        LazyRead Lazy;
+        DataFrame(string csvfile){
+            //public lazy read the data
+            //iterate through rows
+            //create n_col number of dynamic arrays with length n_cols gotten from lazy_read
+        }
+
+};
---
+++ DataFrame/Series.cpp
@@ -0,0 +1,11 @@
+#include"../reader/lazy_read.h"
+
+class Series{
+    private:
+        unsigned long int arrity=0;
+    public:
+        Series(){
+        
+        }
+
+}
